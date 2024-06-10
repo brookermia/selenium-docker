6b20c475ec008baaafe7737447dd8a674a2c851f
Kiana Pazdernik <kianapaz021@gmail.com>
2024-06-10 12:05:14 -0700

    Remove multiple entries

--- app/src/Program.cs
+++ app/src/Program.cs
@@ -2,11 +2,11 @@ namespace SeleniumDocker
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            bool headless = args.Length > 0 && args[0] == "--headless";
-            var test = new TestRdbrck(headless);
-            test.TestRdbrckHomepage();
-        }
+        // public static void Main(string[] args)
+        // {
+        //     bool headless = args.Length > 0 && args[0] == "--headless";
+        //     var test = new TestRdbrck(headless);
+        //     test.TestRdbrckHomepage();
+        // }
     }
 }
